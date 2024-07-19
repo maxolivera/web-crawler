2af7ac5436f0bf3dd8769f42ca7415b9012d98ff
Maximo Olivera <maximo.a.olivera@gmail.com>
2024-07-19 11:58:28 -0300

    added CLI 1st argument as baseURL

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+
+
+function main() {
+	if (Bun.argv.length !== 3){
+		console.log("Invalid amount of CLI arguments")
+		return
+	}
+	let baseURL = null
+	try {
+		baseURL = URL(Bun.argv[2]);
+	} catch (err) {
+		console.log(`${err.message}: ${Bun.argv[2]}`);
+	}
+	console.log(`Taking as baseURL the following '${baseURL}'`)
+}
+
+main()
